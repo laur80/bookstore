ed4309df22dca64b592a812c5361775a8d53e498
laur80 <laur80raul@gmail.com>
2021-03-04 18:29:14 +0200

    edit book fix

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -1,159 +1,149 @@
-import { useState } from 'react';
-import { useQuery } from 'react-query';
-import { useRouter } from 'next/router';
-import { useFormik } from 'formik';
-import axios from 'axios';
-import { Input,FormControl, FormLabel,Textarea,Button,Center, Stack,Form,Wrap,Spinner} from "@chakra-ui/react";
-
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import axios from "axios";
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  Textarea,
+  Button,
+  Center,
+  Stack,
+  Form,
+  Wrap,
+  Spinner,
+} from "@chakra-ui/react";
 
 export async function getServerSideProps(context) {
+  const id = context.params.id.toString();
 
-   const id = context.params.id.toString();
-  
+  if (!id) {
+    return {
+      notFound: true,
+    };
+  }
 
-   if (!id) {
-      return {
-        notFound: true,
-      }
-    }
-  
-   return {
-      props:{id}
-   }
+  return {
+    props: { id },
+  };
 }
 
+export default function newBook({ id }) {
+  const [state, setState] = useState();
+  const router = useRouter();
 
-export default function newBook ({id}) {
-   const router = useRouter();
-   const url =`/api/books/${id}`;
-   let books= {}
-      async function  fetchBook(){
-         let res = await fetch(url)
-       //   console.log('in',bk.data);
-       const bk= await res.json();
-       books = {title: bk.data.title,
-         author:bk.data.author,
-         description:bk.data.description }
-        
-         return books
-      }
+  let books = {};
+  async function fetchBook() {
+    const url = `/api/books/${id}`;
+    let res = await fetch(url);
+    const bk = await res.json();
+    books = {
+      title: bk.data.title,
+      author: bk.data.author,
+      description: bk.data.description,
+    };
 
-  
-   const{data,isPreviousData, status} = useQuery(['book'], fetchBook, {
-    keepPreviousData: true
-  } );
-  
-  const [state, setState] = useState([])
+    return books;
+  }
 
-  if(status === 'success' && state.length ===0){
-   setState (data)
-  } 
-//   console.log(state);
+  const { data, status, isFetching, isSuccess } = useQuery(["book"], fetchBook);
 
-   function handleChange(e){
-      
-      setState({...state,[e.target.name]:e.target.value })
-   }
-
-   const updateRedirect = async() => {
-      const url =`/api/books/${id}`;
-      const updateBook = await axios.put(url,state);
-   }
-
-
-    function handleSubmit(){
-      updateRedirect();
-      router.push('/');
+  useEffect(() => {
+    if (data) {
+      setState(data);
     }
-    
- 
-//    const validate = values => {
-//       const errors = {}
-  
-//       if (!values.title) {
-//          errors.title = 'Title is required';
-//      }
-//      if (!values.author) {
-//       errors.author = 'Author is required';
-//   }
-//      if (!values.description) {
-//       errors.description = 'Description is required';
-//      }
-  
-//       return errors
-//     }
-   
-    return (
-       <>
+  }, [data]);
 
-            {status === 'loading' && (
-                     <Center mt='40'>
-                        <Spinner
-                        thickness="4px"
-                        speed="0.65s"
-                        emptyColor="gray.200"
-                        color="blue.500"
-                        size="xl"
-                        />
-                     </Center>
-            )}
+  //   console.log("state", state);
+  //   console.log("data", data);
 
-            {status === 'success' && (  
-            <Wrap bg='#385898' my='36' w='60%' mx='auto' p='5rem' borderRadius='lg'>
-                  <Stack mx='auto' w='80%'>
-                        <form onSubmit={handleSubmit}>
-                        <FormLabel htmlFor="title" color='whiteAlpha.900'>Book title</FormLabel>
-                        <Input
-                           bg='white'
-                           id="title"
-                           name="title"
-                           type="text"
-                           onChange={handleChange}
-                           // onBlur={handleBlur}
-                           value={state.title}
-                        />
-                        {/* {touched.title && errors.title ? (
-                           <div>{errors.title}</div>
-                        ) : null}
-                  */}
-                        <FormLabel htmlFor="author" color='whiteAlpha.900'>Author Name</FormLabel>
-                        <Input
-                           bg='white'
-                           id="author"
-                           name="author"
-                           type="text"
-                           onChange={handleChange}
-                           // onBlur={handleBlur}
-                           value={state.author}
-                        />
-                        {/* {touched.author && errors.author ? (
-                           <div>{errors.author}</div>
-                        ) : null}
-                  */}
-                        <FormLabel htmlFor="description" color='whiteAlpha.900'>Book Description</FormLabel>
-                        <Textarea
-                           mb='8'
-                           h='40'
-                           p='2'
-                           bg='white'
-                           id="description"
-                           name="description"
-                           type="description"
-                           onChange={handleChange}
-                           // onBlur={handleBlur}
-                           value={state.description}
-                        />
-                        {/* {touched.description && errors.description ? (
-                           <div>{errors.description}</div>
-                        ) : null} */}
-                        <Center>
-                           <Button colorScheme="whatsapp" type="submit">Edit Book</Button>
-                        </Center>   
-                     
-                     </form>
-                  </Stack>
-            </Wrap>
-            )}
-      </>
-      )
+  function handleChange(e) {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  const updateRedirect = async () => {
+    const url = `/api/books/${id}`;
+    const updateBook = await axios.put(url, state);
+    console.log(updateBook);
+  };
+
+  function handleSubmit() {
+    updateRedirect();
+    router.push("/");
+  }
+
+  if (!state) {
+    return <p>No bOOk yet..</p>;
+  }
+
+  return (
+    <>
+      {status === "loading" && (
+        <Center mt='40'>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </Center>
+      )}
+
+      {status === "success" && (
+        <Wrap bg='#385898' my='36' w='60%' mx='auto' p='5rem' borderRadius='lg'>
+          <Stack mx='auto' w='80%'>
+            <form onSubmit={handleSubmit}>
+              <FormLabel htmlFor='title' color='whiteAlpha.900'>
+                Book title
+              </FormLabel>
+              <Input
+                bg='white'
+                id='title'
+                name='title'
+                type='text'
+                onChange={handleChange}
+                value={state.title}
+              />
+
+              <FormLabel htmlFor='author' color='whiteAlpha.900'>
+                Author Name
+              </FormLabel>
+              <Input
+                bg='white'
+                id='author'
+                name='author'
+                type='text'
+                onChange={handleChange}
+                value={state.author}
+              />
+
+              <FormLabel htmlFor='description' color='whiteAlpha.900'>
+                Book Description
+              </FormLabel>
+              <Textarea
+                mb='8'
+                h='40'
+                p='2'
+                bg='white'
+                id='description'
+                name='description'
+                type='description'
+                onChange={handleChange}
+                value={state.description}
+              />
+
+              <Center>
+                <Button colorScheme='whatsapp' type='submit'>
+                  Edit Book
+                </Button>
+              </Center>
+            </form>
+          </Stack>
+        </Wrap>
+      )}
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,71 @@
-import Head from 'next/head'
-import { Center, Spinner, Stack, Skeleton, Wrap} from "@chakra-ui/react";
-import Book from '../components/Book';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { useQuery} from 'react-query';
-// import axios from 'axios';
-
-
-// const HOSTNAME=process.env.HOSTNAME || 'localhost';
-// const PORT = process.env.PORT || 3000;
-// const url ='http://{HOSTNAME}:{PORT}/api/books';
-
+import Head from "next/head";
+import { Center, Spinner, Stack, Skeleton, Wrap } from "@chakra-ui/react";
+import Book from "../components/Book";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [books, setBooks] = useState();
 
-   async function fetchBooks(){
-      const url = '/api/books'
-      const res = await fetch(url)
-      const dt= await res.json();
-      return dt
-   }
-      
+  async function fetchBooks() {
+    const url = "/api/books";
+    const res = await fetch(url);
+    const dt = await res.json();
+    return dt;
+  }
 
-   const{data,isPreviousData, status} = useQuery(['books'], fetchBooks, {
-      keepPreviousData: true
-    } );
-//   if(status=== 'success') console.log(data.data);
+  const { data, status } = useQuery(["books"], fetchBooks, {
+    keepPreviousData: true,
+  });
+  //   if(status=== 'success') console.log(data.data);
 
+  useEffect(() => {
+    if (data) {
+      setBooks(data.data);
+    }
+  }, [data]);
+
+  //   console.log("books", books);
+
+  if (status === "error") {
+    return <p>Error fetching data</p>;
+  }
+
+  if (!books) {
+    return <p>No books yet...</p>;
+  }
 
   return (
-   <>
-   
-      {status === 'loading' && (
-         <Center mt='40'>
-            <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
+    <>
+      {status === "loading" && (
+        <Center mt='40'>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </Center>
+      )}
+
+      {status === "success" && (
+        <div id='container'>
+          {books.map((b) => (
+            <Book
+              key={b._id}
+              title={b.title}
+              description={b.description}
+              author={b.author}
+              id={b._id}
+              b={b}
             />
-         </Center>
+          ))}
+        </div>
       )}
 
-      {status === 'error' && (
-        <div>Error fetching data</div>
-      )}
-
-
-      {status === 'success' && 
-      <div id='container'>
-         {data.data.map((b)=><Book key={b._id} title={b.title} description={b.description} author={b.author} id={b._id} b={b}/>)}
-      </div>}
-    
       <ReactQueryDevtools initialIsOpen={false} />
-    </>  
-  )
+    </>
+  );
 }
